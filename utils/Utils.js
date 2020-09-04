1a4d64458a24a40333f51529af86f1dd173a0d9b
陈晓凡 <bobo343195246@qq.com>
2020-09-04 11:57:18 +0800

    更新: Utils.js 新增 日期格式化函数

--- utils/Utils.js
+++ utils/Utils.js
@@ -12,3 +12,29 @@ export function getHttpQuery() {
   }
   return param;
 }
+
+/**
+ * 日期格式化函数
+ * @param {Date} date 日期函数
+ */
+function getFormatTime(date) {
+  const formatTime = (date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
+
+    return (
+      [year, month, day].map(formatNumber).join("-") +
+      " " +
+      [hour, minute, second].map(formatNumber).join(":")
+    );
+  };
+
+  const formatNumber = (n) => {
+    n = n.toString();
+    return n[1] ? n : "0" + n;
+  };
+}
